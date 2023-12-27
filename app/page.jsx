07a20b0ce37bef4a21e0f0231ad6d7deb24bde34
Lazycoder26 <Rudra26_2004@outlook.com>
2023-12-27 21:59:42 +0530

    Update padding and font weights in CSS, add navigation elements in page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,14 +1,61 @@
+"use client";
 import Link from "next/link";
+import { useState } from "react";
 
 const page = () => {
+  const [highlighted, setHighlighted] = useState("All");
+
   return (
     <main>
       <div className="nav">
         <div className="nav-name">
-          <p className="nav-txt">rudr</p>
+          <p className="nav-txt">Rudr</p>
         </div>
         <div>
-          <nav className=""></nav>
+          <nav className="main-nav">
+            <ul className="nav-ele">
+              <li
+                className={`${
+                  highlighted === "All" ? "highlighted" : ""
+                } elements`}
+                onClick={() => {
+                  setHighlighted("All");
+                }}
+              >
+                All
+              </li>
+              <li
+                className={`${
+                  highlighted === "About" ? "highlighted" : ""
+                } elements`}
+                onClick={() => {
+                  setHighlighted("About");
+                }}
+              >
+                About
+              </li>
+              <li
+                className={`${
+                  highlighted === "Projects" ? "highlighted" : ""
+                } elements`}
+                onClick={() => {
+                  setHighlighted("Projects");
+                }}
+              >
+                Projects
+              </li>
+              <li
+                className={`${
+                  highlighted === "Go-To" ? "highlighted" : ""
+                } elements`}
+                onClick={() => {
+                  setHighlighted("Go-To");
+                }}
+              >
+                Go-To
+              </li>
+            </ul>
+          </nav>
         </div>
         <div>
           <Link href="mailto:rudr26@proton.me" className="mail-link">
