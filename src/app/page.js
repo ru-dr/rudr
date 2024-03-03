eb8a1458218b4b69e95de5f46ce5b2829ca10179
Lazycoder26 <Rudra26_2004@outlook.com>
2024-03-03 16:46:38 +0530

    Remove unused dependencies and code

--- src/app/page.js
+++ src/app/page.js
@@ -1,26 +1,11 @@
-'use client'
 import About from "./components/About";
 import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
 import Socials from "./components/Socials";
-import Lenis from '@studio-freight/lenis'
-import { useEffect } from "react";
-
-
 
 export default function Home() {
-  useEffect(() => {
-    const lenis = new Lenis()
-
-    function raf(time) {
-      lenis.raf(time)
-      requestAnimationFrame(raf)
-    }
-
-    requestAnimationFrame(raf)
-  }, [])
 
   return (
     <main className="p-8 flex gap-y-5 flex-col">
