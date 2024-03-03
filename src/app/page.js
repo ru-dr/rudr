6dd178613d9f91f7738335d4dcd613b215721ccd
Lazycoder26 <Rudra26_2004@outlook.com>
2024-03-03 19:37:03 +0530

    Remove unused code and commented out console.log statement

--- src/app/page.js
+++ src/app/page.js
@@ -8,12 +8,7 @@ import Socials from "./components/Socials";
 import Lenis from '@studio-freight/lenis'
 
 export default function Home() {
-
   const lenis = new Lenis()
-
-  lenis.on('scroll', (e) => {
-    // console.log(e)
-  })
 
   function raf(time) {
     lenis.raf(time)
